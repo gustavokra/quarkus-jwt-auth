3bf6dee158920fedc008540576151a307ec7b4fa
gustavokra <gustavo.kraemer6@gmail.com>
2024-11-14 09:18:06 -0300

    finalizando com images docker

--- generate_jwtKeys.sh
+++ generate_jwtKeys.sh
@@ -1,5 +1,5 @@
 mkdir jwt
-openssl genrsa -out jwt/rsaPrivateKey.pem 2048 -ii
+openssl genrsa -out jwt/rsaPrivateKey.pem 2048
 openssl rsa -pubout -in jwt/rsaPrivateKey.pem -out jwt/publicKey.pem
 openssl pkcs8 -topk8 -nocrypt -inform pem -in jwt/rsaPrivateKey.pem -outform pem -out jwt/privateKey.pem
 chmod 600 jwt/rsaPrivateKey.pem
